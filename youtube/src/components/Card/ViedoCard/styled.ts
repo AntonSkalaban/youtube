d0c9ee1f1305588261e829d68026a95faecc3e06
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2024-03-05 22:51:21 +0300

    feat: add images

--- youtube/src/components/Card/ViedoCard/styled.ts
+++ youtube/src/components/Card/ViedoCard/styled.ts
@@ -50,7 +50,7 @@ export const CardImg = styled.img`
 
   &:before {
     content: "";
-    background-image: url("https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg");
+    background-image: url("assets/jpg/no-image.jpg");
     background-size: cover;
     background-position: center;
     position: absolute;
