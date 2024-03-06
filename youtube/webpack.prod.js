ceaa6799447b15e8a4ff92ce73512f74b75df84a
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2024-03-06 16:20:57 +0300

    fix: fix webpack build sctipt

--- youtube/webpack.prod.js
+++ youtube/webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
-const common = require("./webpack.config");
+const common = require("./webpack.common");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
